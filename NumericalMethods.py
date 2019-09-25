97f4ea8ce235266d4934e6ceff4e68c29b904f4b
Justas327 <justas.milisiunas@gmail.com>
2019-09-25 11:01:53 +0300

    Refactored code
    Refactored code. Added multiple roots finding

--- NumericalMethods.py
+++ NumericalMethods.py
@@ -1,3 +1,6 @@
+from FunctionScanner import FunctionScanner
+
+
 class NumericalMethod:
     def __init__(self, precision, eps, alpha, max_iterations, function, derivative,
                  function_x_values, graph):
@@ -10,10 +13,14 @@ class NumericalMethod:
         self.function_x_values = function_x_values
         self.graph = graph
 
+        scanner = FunctionScanner()
+        self.intervals = scanner.scan(f=self.function, start_x=self.function_x_values[0],
+                                      end_x=self.function_x_values[-1])
+
     def simple_iteration_method(self):
-        x = self.function_x_values[0]
-        iteration = 0  # Iteration count
-        prec = self.precision
+        # x = self.function_x_values[0]
+        # iteration = 0  # Iteration count
+        # prec = self.precision
 
         self.graph.clear_graph()
         self.graph.show_function(self.function_x_values,
@@ -21,32 +28,34 @@ class NumericalMethod:
                                  'b', 'f(x)')
         self.graph.show_function(self.function_x_values, self.function_x_values, 'r', 'y')
 
-        while prec > self.eps:
-            iteration += 1
-            if iteration == self.max_iterations:
-                print("Reached max iterations limit")
-                return
+        for interval in self.intervals:
+            x = interval[0]
+            iteration = 0  # Iteration count
+            prec = self.precision
+            while prec > self.eps:
+                iteration += 1
+                if iteration == self.max_iterations:
+                    print("Reached max iterations limit")
+                    return
 
-            if x > self.function_x_values[-1]:
-                print('X out of bounds')
-                return
+                if x > self.function_x_values[-1]:
+                    print('X out of bounds')
+                    return
 
-            next_x = self.function(x) / self.alpha + x
-            self.graph.show_function([x, x, next_x], [x, next_x, next_x], 'g-')
+                next_x = (self.function(x) / self.alpha) + x
+                self.graph.show_function([x, x, next_x], [x, next_x, next_x], 'g-')
 
-            prec = abs(x - next_x)
-            x = next_x
+                prec = abs(x - next_x)
+                x = next_x
 
-        print(f'Rastos saknys {x}')
-        return x
+            print(f'Rastos saknys {x}')
+            break
 
     def scanning_method(self):
-        prec = 0.1
-        step = 0.1
-        current = self.function_x_values[0]
-        is_positive = self.function(current) > 0
-        changed = False
-        current_iteration = 0
+        # current = self.function_x_values[0]
+        # is_positive = self.function(current) > 0
+        # changed = False
+        # current_iteration = 0
 
         self.graph.clear_graph()
         self.graph.show_function(self.function_x_values, [self.function(x) for x in self.function_x_values], 'b',
@@ -54,48 +63,61 @@ class NumericalMethod:
         self.graph.show_function(self.function_x_values[0], 0, color='ro')
         self.graph.show_function(self.function_x_values[-1], 0, color='ro')
 
-        while prec > self.eps:
-            if current_iteration >= self.max_iterations:
-                print("Reached max iterations limit")
-                return
-            y = self.function(current)
-            if is_positive is not (y > 0):
-                current -= step
-                step /= 2
-                self.graph.show_function(current, 0, color='bo')
-                changed = True
-            else:
-                # time.sleep(0.1)
-                if changed is False:
-                    self.graph.show_function(current, 0, color='yo')
+        for interval in self.intervals:
+            prec = 0.1
+            step = 0.01
 
-                changed = False
-                current += step
+            current = interval[0]
+            is_positive = self.function(current) > 0
+            changed = False
+            current_iteration = 0
 
-            prec = abs(y)
+            while prec > self.eps and (interval[0] <= current <= interval[1]):
+                if current_iteration >= self.max_iterations:
+                    print("Reached max iterations limit")
+                    return
+                y = self.function(current)
+                if is_positive is not (y > 0):
+                    current -= step
+                    step /= 2
+                    self.graph.show_function(current, 0, color='bo')
+                    changed = True
+                else:
+                    # time.sleep(0.1)
+                    if changed is False:
+                        self.graph.show_function(current, 0, color='yo')
 
-            is_positive = y > 0
+                    changed = False
+                    current += step
 
-        print(f"Rasktos saknys: {current}")
+                prec = abs(y)
+
+                is_positive = y > 0
+            print(f"Rasktos saknys: {current}")
 
     def newton_method(self):
-        prec = 0.1
+        # prec = 0.1
 
-        x = (self.function_x_values[0] + self.function_x_values[-1]) / 2
-        current_iteration = 0
+        # x = (self.function_x_values[0] + self.function_x_values[-1]) / 2
+        # current_iteration = 0
 
         self.graph.clear_graph()
         self.graph.show_function(self.function_x_values, [x for x in self.function_x_values], 'b', 'f(x)')
 
-        while prec > self.eps:
-            if current_iteration >= self.max_iterations:
-                print("Reached max iterations limit")
-                return
+        for interval in self.intervals:
+            prec = 0.1
+            x = (interval[0] + interval[-1]) / 2
+            current_iteration = 0
 
-            next_x = (x - self.function(x) / self.derivative(x))
-            prec = abs(next_x - x)
+            while prec > self.eps:
+                if current_iteration >= self.max_iterations:
+                    print("Reached max iterations limit")
+                    return
 
-            x = next_x
-            current_iteration += 1
+                next_x = (x - self.function(x) / self.derivative(x))
+                prec = abs(next_x - x)
 
-        print(f"Rasktos saknys: {x}")
+                x = next_x
+                current_iteration += 1
+
+            print(f"Rasktos saknys: {x}")
