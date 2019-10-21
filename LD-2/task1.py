5b99d6e573e7a669d0b8e88269cb2a5b3e3a09e9
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-10-21 12:53:13 +0300

    Finished 1 task

---
+++ LD-2/task1.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def projection(u, w):
+    return (np.vdot(u, w) / (np.linalg.norm(w)) ** 2) * w
+
+
+def qr_decomposition(a):
+    # Get dimension of matrix.
+    n = len(a)
+    # Create a copy of the matrix.
+    cp = a.copy()
+
+    # Initialize 2 matrices Q and R.
+    Q = np.zeros(shape=(n, n))
+    R = np.zeros(shape=(n, n))
+
+    for i in range(n):
+        u = a[:, i]
+        u = u.astype('float32')
+        w = u
+        # Apply GS method.
+        for k in range(i):
+            u -= projection(w, Q[:, k])
+        Q[:, i] = normalize(u)
+        # Fill R at correct position.
+        for j in range(i + 1):
+            R[j, i] = np.vdot(Q[:, j], cp[:, i])
+
+    # Return results.
+    return Q, R
+
+
+# Solves Ax=B
+def solve(q, r, b):
+    y = np.dot(q.T, b)
+    n = len(r)
+    x = np.zeros((4, 1))
+    for i in range(n - 1, -1, -1):
+        sum = 0
+        for j in range(len(r[i]) - 1, i, -1):
+            sum += r[i, j] * x[j]
+        x[i] = (y[i] - sum) / r[i, i]
+
+    return x
+
+
+# Solves system using numpy
+def check_with_numpy(a, b):
+    q, r = np.linalg.qr(a)
+    _y = np.dot(q.T, b)
+    return np.linalg.solve(r, _y)
+
+
+# Checks if all x satisfy all equations in the system by inserting them in each equation
+def check_x(a, b, x):
+    for i in range(0, len(a) - 1):
+        _sum = 0
+        for j in range(0, len(a[i])):
+            _sum += a[i, j] * x[j]
+
+        if abs(b[i] - _sum) > eps:
+            return False
+
+    return True
+
+
+A = np.array([[1, 1, 1, 1],
+              [1, -1, -1, 1],
+              [2, 1, -1, 2],
+              [3, 1, 2, -1]])
+
+B = np.array([2, 0, 9, 7])
+
+eps = 1e-4
+
+Q, R = qr_decomposition(A.copy())
+y = np.dot(Q.T, B)
+temp = np.linalg.solve(R, y)
+x = solve(Q, R, B.copy())
+print(f"x = {x}")
+
+x_correct = check_x(A, B, x)
+print(f"Is x satisfy all equations in the system: {x_correct}\n")
+
+x_check = check_with_numpy(A, B)
+print(f"Solved with numpy: x = {x_check}")
