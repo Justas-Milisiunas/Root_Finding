217968edda7b4522e11eba00a72525f740fe9680
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-26 19:42:24 +0200

    Added method's for 3 task

--- LD-3/interpolation.py
+++ LD-3/interpolation.py
@@ -93,3 +93,99 @@ def akima_points_derivative(range_x, range_y):
                 np.abs(m(i + 1) - m(i)) + np.abs(m(i - 1) - m(i - 2))))
 
     return range_s
+
+
+def U(start, end, x):
+    """
+    Calculates U value for hermite spline
+    :param start: Interval start
+    :param end: Interval end
+    :param x: Current x
+    :return: Calculated U for given x
+    """
+    return (1 - 2 * (1 / (start - end)) * (x - start)) * ((x - end) / (start - end)) ** 2
+
+
+def V(start, end, x):
+    """
+    Calculates V value for hermite spline
+    :param start: Interval start
+    :param end: Interval end
+    :param x: Current x
+    :return: Calculated V for given x
+    """
+    return (x - start) * ((x - end) / (start - end)) ** 2
+
+
+def hermite_interpolation_spline(range_x, range_y):
+    """
+    Calculates hermite interpolation spline function
+    :param range_x: All x values
+    :param range_y: All y values
+    :return: Hermite interpolation spline function
+    """
+    range_dy = points_slopes(range_x, range_y)
+
+    def spline_function(x):
+        index = np.searchsorted(range_x, x)
+        try:
+            result = U(range_x[index - 1], range_x[index], x) * range_y[index - 1] + V(range_x[index - 1],
+                                                                                       range_x[index],
+                                                                                       x) * range_dy[index - 1] \
+                     + U(range_x[index], range_x[index - 1], x) * range_y[index] \
+                     + V(range_x[index], range_x[index - 1], x) * range_dy[index]
+        except TypeError:
+            # Handles None data from given country's data
+            return None
+        return result
+
+    return spline_function
+
+
+def slope(x1, y1, x2, y2):
+    """
+    Gets given interval slope
+    :param x1: Interval start x
+    :param y1: Interval start y
+    :param x2: Interval end x
+    :param y2: Interval end y
+    :return: Calculated slope
+    """
+
+    return (y2 - y1) / (x2 - x1)
+
+
+def points_slopes(range_x, range_y):
+    """
+    Calculates slopes for each interval between given points
+    :param range_x: x values
+    :param range_y: y values
+    :return: Slopes array
+    """
+
+    slopes = []
+    for i in range(len(range_x) - 1):
+        if range_x[i] is None or range_x[i + 1] is None or range_y[i] is None or range_y[i + 1] is None:
+            slopes.append(None)
+        else:
+            slopes.append(slope(range_x[i], range_y[i], range_x[i + 1], range_y[i + 1]))
+
+    slopes.append(slopes[-1])
+    return slopes
+
+
+def parametric_interpolation(fx, fy, range_t):
+    """
+    Combines fx and fx to result in parametric function
+    :param fx: FX
+    :param fy: FY
+    :param range_t: time range
+    :return: x values array, y values array
+    """
+    x_results = []
+    y_results = []
+    for t in range_t:
+        x_results.append(fx(t))
+        y_results.append(fy(t))
+
+    return x_results, y_results
